a1e810d2baa9faf883c1f4afeb7b82a423942ede
Sophie Brooker <sbrooker@aeo3.ca>
2024-07-03 16:23:20 -0400

    changed file structure added class AreaField

---
+++ src/com/compsci/four/AreaField.java
@@ -0,0 +1,8 @@
+package com.compsci.four;
+import java.util.Scanner;
+public class AreaField {
+
+
+    public static void main(String[] args) {
+    }
+}
--- src/com/compsci/four/TaxTip.java
+++ src/com/compsci/four/TaxTip.java
@@ -1,21 +1,20 @@
 // Tax should be 13%
 // Tip should be 18%
-package com.compsci.four.taxtip ;
+package com.compsci.four;
 
 import java.util.Scanner;
-public class Main {
+public class TaxTip {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the mealprice");
+        System.out.print("Enter the meal price\n");
         double mealprice = scanner.nextDouble();
         double tax=0.13*mealprice;
         double tip=(mealprice)*0.18;
         double totalbill=tip+tax+mealprice;
 
-        System.out.printf("cost of mealprice:%f\n",mealprice);
-        System.out.printf("tax:%f\n",tax);
-        System.out.printf("tip:%f\n",tip);
-        System.out.printf("totalbill:%f\n",totalbill);
-
+        System.out.printf("Subtotal:\t %.2f\n",mealprice);
+        System.out.printf("Tax:\t\t %.2f\n",tax);
+        System.out.printf("Tip:\t\t %.2f\n",tip);
+        System.out.printf("Total Bill:\t %.2f\n",totalbill);
     }
 }
