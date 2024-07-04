8eb73e58a101e503e095914280fe889b7726703a
Sophie Brooker <sbrooker@aeo3.ca>
2024-07-04 11:05:07 -0400

    Added question as comment to each class

--- src/com/compsci/four/AreaField.java
+++ src/com/compsci/four/AreaField.java
@@ -1,3 +1,11 @@
+/*
+Program #2: Area of a Field:
+
+Create a program that reads the length and width of a farmer’s field from the user in
+feet. Display the area of the field in acres.
+Hint: There are 43,560 square feet in once acre.
+*/
+
 package com.compsci.four;
 
 import java.util.Scanner;
--- src/com/compsci/four/TaxTip.java
+++ src/com/compsci/four/TaxTip.java
@@ -1,5 +1,15 @@
-// Tax should be 13%
-// Tip should be 18%
+/*
+Program #1: Tax and Tip
+
+        The program that you create for this exercise will begin by reading the cost of a meal
+        ordered at a restaurant from the user. Then your program will compute the tax and tip
+        for the meal. Use 13% when computing the amount of tax owing. Compute the tip as
+        18% of the meal amount (before the tax). The output from your program should include the tax amount,
+        the tip amount, and the grand total for the meal including both the tax and the tip.
+
+        Format the output so that all of the values are displayed using two decimal places.
+ */
+
 package com.compsci.four;
 
 import java.util.Scanner;
@@ -17,6 +27,6 @@ public class TaxTip {
         System.out.printf("Subtotal:\t %.2f\n",mealprice);
         System.out.printf("Tax:\t\t %.2f\n",tax);
         System.out.printf("Tip:\t\t %.2f\n",tip);
-        System.out.printf("Total Bill:\t %.2f\n",totalbill);
+        System.out.printf("Total Bill:\t %.2f",totalbill);
     }
 }
