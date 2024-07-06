916fcb460678ade844062818886fd43a5bf72e1a
Sophie Brooker <sbrooker@aeo3.ca>
2024-07-06 18:56:44 -0400

    adding stubbed classes

---
+++ src/com/compsci/three/Program1FindMaximumValueArray.java
@@ -0,0 +1,21 @@
+///*
+//Write a Java program that takes an array of integers as input and finds
+//the maximum value in the array. Assume the array is not empty.
+// */
+//
+//package com.compsci.three;
+//
+//public class Program1FindMaximumValueArray {
+//    public static void main(String[] args) {
+//        int[] numbers = {3, 5, 7, 2, 8, 6, 4, 10, 1};
+//
+//        for[]
+//
+////        double maxValue = Double.MIN_VALUE;
+////        double minValue = Double.MAX_VALUE;
+////
+////        int max = numbers[10];
+////        int min = numbers[0];
+//
+//    }
+//}
