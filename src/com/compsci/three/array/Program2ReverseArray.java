ed121b8abfa1abe336115bab73ee1d3ecfbbc42d
Sophie <sbrooker@aeo3.ca>
2024-07-10 16:34:52 -0400

    finished reverse array

--- src/com/compsci/three/array/Program2ReverseArray.java
+++ src/com/compsci/three/array/Program2ReverseArray.java
@@ -3,10 +3,15 @@ Write a Java program that reverses the elements of an array. For example, if the
 the output should be {5, 4, 3, 2, 1}.
  */
 
-//package com.compsci.three;
-//
-//public class Program2ReverseArray {
-//    public static void main(String[] args) {
-//        int[] numbers = {1, 2, 3, 4, 5};
-//    }
-//}
+package com.compsci.three;
+
+public class Program2ReverseArray {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        for (int i = (numbers.length - 1); i > -1  ;  i--) {
+            System.out.println(numbers[i]);
+        }
+
+    }
+}
+
