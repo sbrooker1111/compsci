3d944b2e632b6f6961f6bde16a6f7286bdaaed89
Sophie <sbrooker@aeo3.ca>
2024-07-11 10:47:33 -0400

    finished sum even numbers array

--- src/com/compsci/three/array/Program3SumEvenNumbersArray.java
+++ src/com/compsci/three/array/Program3SumEvenNumbersArray.java
@@ -1,11 +1,20 @@
-///*
-//Write a Java program that calculates the sum of all even numbers in an array of integers.
-// */
-//
-//package com.compsci.three;
-//
-//public class Program3SumEvenNumbersArray {
-//    public static void main(String[] args) {
-//        int[] number = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-//    }
-//}
+/*
+Write a Java program that calculates the sum of all even numbers in an array of integers.
+ */
+
+package com.compsci.three.array;
+
+public class Program3SumEvenNumbersArray {
+    public static void main(String[] args) {
+
+        int[] number = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int sum = 0;
+        for (int i = 0; i < (number.length -1); i++) {
+        if (number[i] % 2 == 0) {
+            sum += number[i];
+            System.out.println(sum);
+        }
+        }
+        System.out.println(sum);
+    }
+}
