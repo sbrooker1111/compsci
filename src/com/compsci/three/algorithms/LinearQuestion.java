135d0f1f223c79ad5e14c66cb169af13cdb4fa10
Sophie <sbrooker@aeo3.ca>
2024-07-10 22:07:00 -0400

    fixed whitespace

--- src/com/compsci/three/algorithms/LinearQuestion.java
+++ src/com/compsci/three/algorithms/LinearQuestion.java
@@ -12,19 +12,19 @@ public class LinearQuestion {
         int[] archer  = {9,1,8,2,7,3,6,4,5};
         int index = linearSearch(archer, 1);
 
-        if(index != -1) {
-            System.out.println("Element found at index " + index);
-        }
-        else{
+        if (index == -1) {
             System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index " + index);
         }
     }
 
     private static int linearSearch(int[] arr, int sought) {
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i] == sought) {
-                return i;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != sought) {
+                continue;
             }
+            return i;
         }
         return -1;
     }
